882bbde564640eafd8a8d8c68a40705d0d938b38
George Petri <t8_8t@icloud.com>
2021-12-30 18:14:40 +0200

    map tastyworks csv completely

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ pub struct FileUploadForm<'f> {
 
 //todo nice error handling
 //todo does this need to be async?
+//todo add tests
+//todo redirect index
 #[post("/upload", data = "<form>")]
 pub async fn upload(form: Form<FileUploadForm<'_>>) {
     match form.file.path() {
@@ -38,11 +40,37 @@ fn rocket() -> Rocket<Build> {
         .mount("/", FileServer::from("static/"))
 }
 
-//todo add everything in the proper type
 #[derive(Debug, Deserialize)]
 struct TastyworksRecord {
+    #[serde(rename = "Date/Time")]
+    date_time: String,
+    //todo change type
+    #[serde(rename = "Transaction Code")]
+    transaction_code: String,
+    #[serde(rename = "Transaction Subcode")]
+    transaction_subcode: String,
+    #[serde(rename = "Symbol")]
+    symbol: Option<String>,
+    #[serde(rename = "Buy/Sell")]
+    buy_sell: Option<String>,
+    #[serde(rename = "Open/Close")]
+    open_close: Option<String>,
+    #[serde(rename = "Quantity")]
+    quantity: i64,
+    #[serde(rename = "Expiration Date")]
+    expiration_date: Option<String>,
+    #[serde(rename = "Strike")]
+    strike: Option<String>,
+    #[serde(rename = "Call/Put")]
+    call_put: Option<String>,
+    #[serde(rename = "Price")]
+    price: Option<String>,
     #[serde(rename = "Fees")]
     fees: String,
-    #[serde(rename = "Price")]
-    price: String,
+    #[serde(rename = "Amount")]
+    amount: String,
+    #[serde(rename = "Description")]
+    description: String,
+    #[serde(rename = "Account Reference")]
+    account_reference: String,
 }
