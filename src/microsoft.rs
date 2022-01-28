3a1e20ef72ab61b3e34c449cfd30ee3e06a9c52f
George Petri <t8_8t@icloud.com>
2022-01-29 00:10:52 +0200

    test code for creating a worksheet

--- src/microsoft.rs
+++ src/microsoft.rs
@@ -108,6 +108,30 @@ pub async fn file_exists(path: &Path, name: &str) -> Result<bool, OrgError> {
     Ok(true)
 }
 
+//todo impl properly
+pub async fn test_worksheet() -> Result<(), OrgError> {
+    let uri = "https://graph.microsoft.com/v1.0/me/drive/root:/org/ledger.xlsx:/content";
+
+    let client = reqwest::Client::new();
+    let response = client
+        .put(uri)
+        .bearer_auth(redis_data::access_token())
+        .header(
+            "Content-Type",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+        .header("Content-Length", 0)
+        .send()
+        .await?;
+
+    let code = response.status();
+
+    println!("code {}", response.status());
+    println!("text {}", response.text().await?);
+
+    Ok(())
+}
+
 pub async fn upload_to_source(path: &Path, name: &str) -> Result<(), OrgError> {
     let file = fs::read(&path)?;
 
